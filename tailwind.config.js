6acd79df168fe72f23b234b344a0f20aef6b1cf8
Choerul <achmadchoerul12345@gmail.com>
2025-06-27 22:15:14 +0700

    build: install and configure Tailwind CSS 3.3.5
    - Add tailwindcss 3.3.5 as dependency
    - Setup tailwind config file
    - Configure postcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./app/Modules/**/Views/**/*.php",
+    "./app/Views/**/*.php",
+    "./public/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
