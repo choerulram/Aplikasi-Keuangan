fab289a2fb973343445896b0283175c3a2362437
Choerul <achmadchoerul12345@gmail.com>
2025-06-27 23:26:32 +0700

    feat(views): setup project estimation views structure

--- app/Controllers/DashboardController.php
+++ app/Controllers/DashboardController.php
@@ -4,10 +4,10 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 
-class Home extends BaseController
+class DashboardController extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        return view('Dashboard/index');
     }
 }
---
+++ app/Views/Dashboard/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+</head>
+<body>
+    <h1>Selamat Datang di Dashboard Aplikasi Keuangan</h1>
+    <p>Ini adalah halaman utama dashboard aplikasi keuangan Anda.</p>
+</body>
+</html>
